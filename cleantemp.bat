65cb65f7ab4c23990c165f6329eb7257bed787f6
Puneet Bansal <59264906+puneet-bansal@users.noreply.github.com>
2021-09-25 12:51:40 +0200

    clean temp bat file

---
+++ cleantemp.bat
@@ -0,0 +1,15 @@
+@echo off
+	del "%tmp%\*.*" /s /q /f
+	FOR /d %%p IN ("%tmp%\*.*") DO rmdir "%%p" /s /q
+
+	del "C:\Windows\Temp\*.*" /s /q /f
+	FOR /d %%p IN ("C:\Windows\Temp\*.*") DO rmdir "%%p" /s /q
+
+	del /F /Q %APPDATA%\Microsoft\Windows\Recent\*
+	del /F /Q %APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*
+	del /F /Q %APPDATA%\Microsoft\Windows\Recent\CustomDestinations\*
+
+	del "C:\Windows\Prefetch\*.*" /s /q /f
+	FOR /d %%p IN ("C:\Windows\Prefetch\*.*") DO rmdir "%%p" /s /q
+
+	tree
